0d3c0eeadcdbafcefa93a541bd6874c3b8721368
Laura Slayton <laura.slayton.19@cnu.edu>
2020-12-18 15:37:49 -0500

    finished and working

--- src/DrinkOrder.cpp
+++ src/DrinkOrder.cpp
@@ -8,3 +8,50 @@
 #include "../includes/DrinkOrder.h"
 #include "../includes/Water.h"
 #include "../includes/Coffee.h"
+
+//TODO use customerName and CustomerDrink to initialize
+//corresponding member variables, initialize p_drink to null
+DrinkOrder::DrinkOrder(std::string customerName, Drinktype customerDrink) :
+		customerName(customerName), customerDrink(customerDrink), p_drink(
+				nullptr) {
+
+}
+
+//TODO cleanup
+DrinkOrder::~DrinkOrder() {
+	if (p_drink) {
+		delete p_drink;
+	}
+}
+
+//TODO compare by customerName
+bool DrinkOrder::operator<(const DrinkOrder &rs) {
+	return customerName < rs.customerName;
+}
+
+//TODO returns the name and the Drinktype associated with this order
+//if p_drink is null returns customerName+" gets nothing"
+//otherwise it returns name +" gets " + the whatami() function of the object that p_drink points to
+std::string DrinkOrder::announceNameAndDrink() {
+	if (p_drink == NULL) {
+		return customerName + std::string(" gets nothing");
+	}
+	return customerName + std::string(" gets ") + p_drink->whatami();
+}
+
+//TODO allocates a new drink based on customerDrink,
+//use p_drink to point to it.  if customerDrink==NONE then set p_drink to null
+//DO NOT USE ANY IF STATEMENTS IN THIS FUNCTION
+void DrinkOrder::makeOrder() {
+	switch (customerDrink) {
+	case NONE:
+		p_drink = nullptr;
+		break;
+	case COFFEE:
+		p_drink = new Coffee();
+		break;
+	case WATER:
+		p_drink = new Water();
+		break;
+	}
+}
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -8,13 +8,34 @@
 #include "../includes/utilities.h"
 #include "../includes/DrinkOrder.h"
 
-void takeOrders(std::vector<std::unique_ptr<DrinkOrder>> &myDrinkOrders  ){
+void takeOrders(std::vector<std::unique_ptr<DrinkOrder>> &myDrinkOrders) {
 	//TODO add the following orders to the vector
 	//	Will,WATER
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Will"), WATER)));
 	//	Allen,COFFEE
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Allen"), COFFEE)));
 	//	Barnes,WATER
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Barnes"), WATER)));
 	//	Elly,COFFEE
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Elly"), COFFEE)));
 	//	Constance,COFFEE
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Constance"), COFFEE)));
 	//	Zane,WATER
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Zane"), WATER)));
 	//  Liz, NONE
+	myDrinkOrders.push_back(
+			std::unique_ptr<DrinkOrder>(
+					new DrinkOrder(std::string("Liz"), NONE)));
 }
